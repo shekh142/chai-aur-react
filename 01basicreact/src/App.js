0f6a5eec9d2b4db53440a2c48682a12db9004019
Shekhar <sbhushan07735@gmail.com>
2024-02-28 17:14:07 +0530

    hooks

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <chai/>
+    <h1>Hello Shekhar</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+let [counter, setCounter] = useState(15)
+
+//let counter = 15
+
+const addValue = () => {
+  console.log("clicked", counter);
+  //counter = counter + 1;
+  setCounter(counter+1)
+}
+
+const removevalue = () => {
+  setCounter(counter-1)
+}
+
+  return (
+    <>
+      
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      
+      <button onClick={addValue}>Add value{counter}</button>
+      <br/>
+      <button onClick={removevalue}> Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
